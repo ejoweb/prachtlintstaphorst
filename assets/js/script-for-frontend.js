1d2bce092d648b9cad2b0366a44b99777b374f29
Erik Joling <info@ejoweb.nl>
2023-05-20 14:01:26 +0200

    Add script to automagically send activitytitle as subject to contactpage

---
+++ assets/js/script-for-frontend.js
@@ -0,0 +1,47 @@
+
+/**
+ * Activities: add heading to meedoen-link for contactform 
+ * 
+ * We gaan uit van de volgende structuur
+ * 
+ * Activiteitengroep 
+ *   - met `id=activiteiten`
+ * Activiteit
+ * 	 - met class `is-style-activity`
+ *   - met inhoud Heading-block
+ *   - met inhoud link naar `meedoen`
+ * Contactpagina
+ *   - met url waarin `meedoen` naar voren komt
+ *   - met contactformulierveld `your-subject`
+ */
+	
+let activitiesGroup = document.querySelector( '#activiteiten' );
+let activities;
+let activity;
+let activityTitle;
+let activityAnchor;
+let activityAnchorLink;
+
+if (activitiesGroup) {
+	
+	let activities = activitiesGroup.querySelectorAll('.is-style-activity');
+
+	if (activities) {
+
+		// Remove negative tabindexes set by methods.preventFocusInTarget()
+		for (var i = 0; i < activities.length; i++) {
+
+			activityTitle = activities[i].querySelector( '.wp-block-heading' );
+			activityAnchor = activities[i].querySelector( 'a[href*="meedoen"]' );
+
+			if (activityTitle && activityAnchor) {
+
+				activityAnchorLink = new URL(activityAnchor.href);
+
+				activityAnchorLink.searchParams.append('your-subject', activityTitle.textContent);
+
+				activityAnchor.href = activityAnchorLink;
+			}
+		}
+	}
+}
